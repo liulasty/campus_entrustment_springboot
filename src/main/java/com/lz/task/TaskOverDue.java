06755b040910fdc26831af296c6fe6ec5ee099eb
liulasty <2312034544@qq.com>
2025-02-22 21:39:42 +0800

    feat(task): 添加 RabbitMQ 消息发送功能
    - 新增 SimpleQueue 方法，定时向 simple.queue 发送消息
    - 添加 testWorkQueue 方法，用于模拟消息堆积
    - 实现 testFanoutExchange 方法，定时向 lz.fanout 交换机发送消息
    - 注释掉原有的 delegateOverDue 方法调度

--- src/main/java/com/lz/task/TaskOverDue.java
+++ src/main/java/com/lz/task/TaskOverDue.java
@@ -13,10 +13,12 @@ import com.lz.mapper.TaskMapper;
 import com.lz.pojo.Enum.TaskStatus;
 import com.lz.pojo.entity.Task;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 
@@ -28,11 +30,14 @@ import java.util.List;
 public class TaskOverDue {
     @Autowired
     private TaskMapper taskMapper;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
     
     @Autowired
     private TaskAcceptRecordsMapper taskAcceptRecordsMapper;
     //todo 委托过期处理
-    @Scheduled(cron = "0/30 * * * * ?")
+//    @Scheduled(cron = "0/30 * * * * ?")
     public void delegateOverDue() {
         log.info("委托过期");
         QueryWrapper<Task> queryWrapper = new QueryWrapper();
@@ -47,5 +52,48 @@ public class TaskOverDue {
         });
         log.info("过期任务：" + tasks);
         
+    }
+
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void SimpleQueue() {
+        // 队列名称
+        String queueName = "simple.queue";
+        // 消息
+        String message = "hello, spring amqp!";
+        // 当前时间
+        Date now = new Date();
+//
+        //使用formDate格式化时间 YYYY-MM-DD HH:mm:ss
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String nowString = now.toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalDateTime().format(formatter);
+        // 发送消息
+        rabbitTemplate.convertAndSend(queueName, message+nowString);
+
+    }
+
+    /**
+     * workQueue
+     * 向队列中不停发送消息，模拟消息堆积。
+     */
+
+    public void testWorkQueue() throws InterruptedException {
+        // 队列名称
+        String queueName = "simple.queue";
+        // 消息
+        String message = "hello, message_";
+        for (int i = 0; i < 50; i++) {
+            // 发送消息
+            rabbitTemplate.convertAndSend(queueName, message + i);
+            Thread.sleep(20);
+        }
+    }
+
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void testFanoutExchange() {
+        // 队列名称
+        String exchangeName = "lz.fanout";
+        // 消息
+        String message = "hello, everyone!";
+        rabbitTemplate.convertAndSend(exchangeName, "", message);
     }
 }
